6048565e9d10be78d3c5cb3afb813df88711d327
max <harumaxy@gmail.com>
2021-01-11 01:43:52 +0900

    add hello world system

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,18 @@
 use bevy::prelude::*;
 
-struct Pos {
-    x: i32,
-    y: i32,
-}
+#[macro_use]
+extern crate bevy_discovery;
 
-struct Obj;
+// bevy_discovery Plugin を Auto Deriving
+// #[system] macro をつけた関数をすべて App にシステムとして追加するマクロ
+#[derive(DiscoveryPlugin)]
+struct DiscoveryPulugin;
 
-fn print_pos_system(query: Query<&Pos>) {
-    for pos in query.iter() {
-        println!("{:?}, {:?}", pos.x, pos.y);
-    }
-}
-
-fn init_system(commands: &mut Commands) {
-    commands.spawn((Obj, Pos { x: 1, y: 2 }));
-}
-
-fn spam_spam_spam_system(query: Query<&Pos>) {
-    query.iter().for_each(|pos| print!("spam"));
-}
-
-fn i_am_the_master_system() {
-    println!("I am the Master!")
+#[system]
+fn hello_world_system() {
+    print!("Hello World")
 }
 
 fn main() {
-    App::build()
-        .add_startup_system(init_system.system())
-        .add_system(print_pos_system.system())
-        .add_system(spam_spam_spam_system.system())
-        .add_system(i_am_the_master_system.system())
-        .run();
+    App::build().add_plugin(DiscoveryPulugin).run()
 }
