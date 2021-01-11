57e557bdcd6c0e444555c0f8406198858d13c6c8
max <harumaxy@gmail.com>
2021-01-11 10:00:00 +0900

    remove system from fn name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-fn hello_world_system() {
+fn hello_world() {
     println!("Hello World")
 }
 
@@ -9,7 +9,7 @@ struct Name(String);
 
 // 基本的に、Component は 引数で コンパイル時に Dynamic に決定される
 // &mut, or & が基本
-fn add_people_to_world_system(commands: &mut Commands) {
+fn add_people_to_world(commands: &mut Commands) {
     let people = (1..=100)
         .into_iter()
         .map(|num| num.to_string())
@@ -22,7 +22,7 @@ fn add_people_to_world_system(commands: &mut Commands) {
         .spawn((Person, Name("Kenes".to_string())));
 }
 
-fn great_people_system(query: Query<(&Person, &Name)>) {
+fn great_people(query: Query<(&Person, &Name)>) {
     for (_, Name(name)) in query.iter() {
         println!("Hello {}", name)
     }
@@ -31,8 +31,8 @@ fn great_people_system(query: Query<(&Person, &Name)>) {
 #[bevy_main]
 fn main() {
     App::build()
-        .add_startup_system(add_people_to_world_system(commands).system())
-        .add_system(hello_world_system.system())
-        .add_system(great_people_system.system())
+        .add_startup_system(add_people_to_world.system())
+        .add_system(hello_world.system())
+        .add_system(great_people.system())
         .run()
 }
