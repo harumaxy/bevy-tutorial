1a9e85ba589e743d2c137ebb6ff4cdbe12c30822
max <harumaxy@gmail.com>
2021-01-11 09:57:13 +0900

    add spwan_batch

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 use bevy::prelude::*;
 
-#[macro_use]
-extern crate bevy_discovery;
-
-// bevy_discovery Plugin を Auto Deriving
-// #[system] macro をつけた関数をすべて App にシステムとして追加するマクロ
-#[derive(DiscoveryPlugin)]
-struct DiscoveryPulugin;
-
-#[system]
 fn hello_world_system() {
-    print!("Hello World")
+    println!("Hello World")
 }
 
+struct Person;
+struct Name(String);
+
+// 基本的に、Component は 引数で コンパイル時に Dynamic に決定される
+// &mut, or & が基本
+fn add_people_to_world_system(commands: &mut Commands) {
+    let people = (1..=100)
+        .into_iter()
+        .map(|num| num.to_string())
+        .map(|name| (Person, Name(name)));
+
+    commands
+        .spawn_batch(people)
+        .spawn((Person, Name("Max".to_string())))
+        .spawn((Person, Name("Hennly".to_string())))
+        .spawn((Person, Name("Kenes".to_string())));
+}
+
+fn great_people_system(query: Query<(&Person, &Name)>) {
+    for (_, Name(name)) in query.iter() {
+        println!("Hello {}", name)
+    }
+}
+
+#[bevy_main]
 fn main() {
-    App::build().add_plugin(DiscoveryPulugin).run()
+    App::build()
+        .add_startup_system(add_people_to_world_system(commands).system())
+        .add_system(hello_world_system.system())
+        .add_system(great_people_system.system())
+        .run()
 }
